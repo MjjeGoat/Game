7893df713f088672d0e0a530b7a533c5fce5e8f0
matej <matejhanzlik25@gmail.com>
2025-02-28 08:21:35 +0100

    chuze po mape 2.0

--- src/Movement.java
+++ src/Movement.java
@@ -8,7 +8,6 @@ public class Movement {
 
     public boolean moving() {
         String where = " ";
-        ArrayList<String> location = new ArrayList<>();
         cm.loadMap();
         while (!where.equals("konec")) {
             for (Location lokace : cm.getMap().values()) {
@@ -16,13 +15,13 @@ public class Movement {
                 if (lokace.getName().equals(cm.getCurrentpos())) {
                     System.out.println("Zadejte kam chcete jit: " + Arrays.toString(lokace.getLocations()));
                     where = sc.nextLine().trim();
-                    for (int i = 0; i < lokace.getLocations().length; i++) {
-                        location.add(lokace.getLocations()[i]);
+                    if (where.equals("konec")) {
+                        break;
                     }
-                    for (int i = 0; i < location.size(); i++) {
-                        if (where.equals(location.get(i))) {
-                            cm.setCurrentpos(location.get(i));
-                            i = location.size();
+                    for (int i = 0; i < lokace.getLocations().length; i++) {
+                        if (where.equals(lokace.getLocations()[i])){
+                            cm.setCurrentpos(lokace.getLocations()[i]);
+                            i = lokace.getLocations().length;
                             a++;
                         }
                     }
