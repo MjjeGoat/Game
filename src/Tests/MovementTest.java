c84eb10a4bce5011134b1966590f44c06b3ab595
matej <matejhanzlik25@gmail.com>
2025-04-01 19:51:17 +0200

    Hotovy Unit testy

---
+++ src/Tests/MovementTest.java
@@ -0,0 +1,19 @@
+package Game.Commands;
+
+import Game.Location;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MovementTest {
+
+    @Test
+    void lockedDoor() {
+        Use use = new Use();
+        Movement move = new Movement(use);
+        use.p.inv.addUsedItem("Kolo");
+        move.cm.setCurrentpos("Testovaci");
+        Location l = new Location(0, "Testovaci", "Kolo", null, null, null, null);
+        assertTrue(move.lockedDoor(l), "Metoda by mela vratit true");
+    }
+}
